c97e3ee7a53e984415f1aef27590017eb5d10c4a
llipe_20 <silvahlipe@gmail.com>
2024-04-21 16:26:24 -0300

    Landing page finalizada

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,25 +1,33 @@
 import React from "react";
 import { HiOutlineSearch } from "react-icons/hi";
 import { HiX } from "react-icons/hi";
+import { FaLinkedinIn, FaWhatsapp, FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
+import { BsCodeSlash } from "react-icons/bs";
+import Volume from "./Volume";
 import '../App.css';
 
 const LandingPage = () => {
     return (
-        <div class="flex flex-col justify-between items-center gap-5 w-screen min-h-screen bg-gray-200" >
-            <section class="flex flex-col justify-start items-center w-full h-80 lg:h-96 backdrop relative">
+        <div class="flex flex-col justify-start items-center w-screen min-h-screen bg-white lg:bg-gray-200" >
+            <header class="flex flex-col justify-start items-center w-full h-80 lg:h-96 backdrop relative">
                 <div class="w-full h-20 bg-yellow-950 opacity-80"></div>
-                {/* Caixa da logo*/}
+                {/* Caixa da logo - NÃO MEXER*/}
                 <div class="flex justify-center items-center w-auto h-20 lg:h-24 lg:h-32 bg-yellow-900 pr-4 lg:pr-6 p-2 lg:p-4 absolute top-0 left-4 lg:left-36 z-10">
                     <img className="w-16 lg:w-24" src="https://i.ibb.co/yFCP6gW/Logo-gawain.png" alt="logo-gawain" />
                     <div className="flex flex-col justify-center items-start">
-                        <span className="text-sm lg:text-lg font-bold text-white">projeto</span>
-                        <span className="text-base lg:text-2xl font-bold text-white">GAWAIN</span>
+                        <span className="text-sm lg:text-lg font-bold text-white sedan">projeto</span>
+                        <span className="text-base lg:text-2xl font-bold text-white sedan">GAWAIN</span>
                     </div>
                 </div>
-                {/* Caixa do usuario */}
+                {/* Caixa do NOME & FOTO do usuário */}
                 <div className="flex justify-end items-center w-1/2 h-16 lg:h-16 gap-3 text-white absolute top-0 lg:top-2 right-4 lg:right-36">
                     <span className="text-sm lg:text-lg">Nome do usuário</span>
-                    <div className="w-10 lg:w-14 h-10 lg:h-14 rounded-full bg-white z-0 cursor-pointer">{/* IMAGEM DO USUARIO AQUI */}</div>
+
+                    {/* Esse é o circulo onde fica a imagem do usuario: colocar o link da foto na tag IMG */}
+                    <div className="w-10 lg:w-14 h-10 lg:h-14 rounded-full bg-white z-0 cursor-pointer">
+                        <img src="" alt="" className="w-10 lg:14 rounded-full" />
+                    </div>
                 </div>
 
                 <div class="flex flex-col justify-between items-center w-full h-72 relative">
@@ -28,13 +36,42 @@ const LandingPage = () => {
                     {/* Campo de pesquisa*/}
                     <div className="flex w-full h-20 lg:h-16 justify-center items-start p-4 lg:p-0">
                         <div className="flex justify-between items-center w-full md:w-1/2 h-10 lg:h-12 text-black lg:text-lg bg-white pl-4 pr-4 rounded-lg overlow-hidden text-white">
-                             <HiOutlineSearch className="cursor-pointer" />
+                             <HiOutlineSearch className="cursor-pointer hover:scale-105" />
                             <input type="text" className="w-11/12 h-10 lg:h-12 border-0 outline-0 pl-3 lg:p-0" placeholder="Pesquisar" />
-                            <HiX className="cursor-pointer" />
+                            <HiX className="cursor-pointer hover:scale-105" />
                         </div>
                     </div>
                 </div>
-            </section>
+            </header>
+            {/* Corpo da landing page*/}
+            <main className="flex flex-col justify-start items-center w-full min-h-96 gap-4 bg-white lg:bg-gray-200">
+                <div className="w-full h-5 bg-yellow-900"></div>
+                <Volume />
+            </main>
+            {/* Rodape da trem*/}
+            <footer className="flex flex-col justify-center items-center w-full h-44 lg:h-52 mt-4">
+                {/* Creditos */}
+                <div className="flex flex-col justify-center items-center w-full h-28 lg:h-40 bg-yellow-900 text-white">
+                    <div class="flex justify-center items-center w-auto h-20 lg:h-24 lg:h-32">
+                        <img className="w-16 lg:w-24" src="https://i.ibb.co/yFCP6gW/Logo-gawain.png" alt="logo-gawain" />
+                        <div className="flex flex-col justify-center items-start">
+                            <span className="text-sm lg:text-lg font-bold text-white sedan">projeto</span>
+                            <span className="text-base lg:text-2xl font-bold text-white sedan">GAWAIN</span>
+                        </div>
+                        
+                    </div>
+                    <span className="mb-4 lg:text-base text-sm w-3/4 text-center">
+                        Criado e desenvolvido por <a href="">Triplice Entente</a> &copy; 2024
+                    </span>
+                </div>
+                <nav className="flex justify-center items-center gap-5 w-full h-12 bg-yellow-800 text-white">
+                    <a class="hover:scale-110" target="_blank" href="https://www.linkedin.com/in/felipe-silva--/"><FaLinkedinIn /></a>
+                    <a class="hover:scale-110" target="_blank" href="https://github.com/llipe20"><FaGithub /></a>
+                    <a class="hover:scale-110" target="_blank" href="https://www.instagram.com/___llpe/"><FaInstagram /></a>
+                    <a class="hover:scale-110" target="_blank" href="https://api.whatsapp.com/qr/O43EG6NVZ3QCE1?autoload=1&app_absent=0"><FaWhatsapp /></a>
+                    <a class="hover:scale-110" target="_blank" href="https://portfolio-felipe-gamma.vercel.app/"><BsCodeSlash /></a>
+                </nav>
+            </footer>
         </div>
     )
 }
---
+++ src/components/Volume.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Volume = () => {
+    return (
+        <div className="flex flex-col gap-3 w-full lg:w-10/12 pl-4">
+            {/* Componente de livros por serie */}
+            <div className="flex justify-start items-center w-full">
+                <h2 className="w-full text-lg lg:text-2xl font-bold border-b-4 border-yellow-900 text-black text-start">
+                    Programação
+                </h2>
+            </div>
+            <section className="flex justify-center items-center w-full min-h-96  mt-2 mb-2">
+                <section className="grid w-full min-h-96 rounded-lg bg-white lg:shadow">
+
+                </section>
+            </section>
+        </div>
+    )
+}
+
+export default Volume
